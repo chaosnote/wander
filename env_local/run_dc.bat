4b3bb6dcf8a2d5be1ecda2946b36c9cc80d15656
chris <yeditor@gmail.com>
2025-06-11 15:05:29 +0800

    1、調整變數

--- env_local/run_dc.bat
+++ env_local/run_dc.bat
@@ -1,21 +1,23 @@
 @echo off
 
+set vb_addr=192.168.0.236
+
 @REM 本地伺服器
 set DC_ID=28c2f2c2-590f-442d-bf80-7e6bbeb4c471
 set DC_ADDR=:8080
 set USE_GUEST=1
 
 @REM DB
-set DB_ADDR=192.168.0.236:3306
+set DB_ADDR=%vb_addr%:3306
 set DB_USER=chris
 set DB_PW=123456
 set DB_NAME=game_dev
 
 @REM NATS
-set NATS_ADDR=192.168.0.236:4222
+set NATS_ADDR=%vb_addr%:4222
 
 @REM REDIS
-set REDIS_ADDR=192.168.0.236:6379
+set REDIS_ADDR=%vb_addr%:6379
 set REDIS_DB_INDEX=0
 
 set proj=%cd%
--- env_local/run_game.bat
+++ env_local/run_game.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set vb_addr=192.168.0.236
+
 @REM DC 伺服器
 set DC_ID=28c2f2c2-590f-442d-bf80-7e6bbeb4c471
 set DC_ADDR=:8080
@@ -9,21 +11,21 @@ set GAME_ADDR=:8081
 set GROUP_ID=00
 
 @REM DB
-set DB_ADDR=192.168.0.236:3306
+set DB_ADDR=%vb_addr%:3306
 set DB_USER=chris
 set DB_PW=123456
 set DB_NAME=game_dev
 
 @REM MONGO
-set MONGO_ADDR=192.168.0.236:27017
+set MONGO_ADDR=%vb_addr%:27017
 set MONGO_USER=admin
 set MONGO_PW=password
 
 @REM NATS
-set NATS_ADDR=192.168.0.236:4222
+set NATS_ADDR=%vb_addr%:4222
 
 @REM REDIS
-set REDIS_ADDR=192.168.0.236:6379
+set REDIS_ADDR=%vb_addr%:6379
 set REDIS_DB_INDEX=0
 
 @REM LOG_DIR
